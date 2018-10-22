db93a68421660bec6c52b5efe543addc6f06f499
Jaime <jaimeconvery@gmail.com>
2018-10-22 14:37:41 -0400

    Public User Login

---
+++ process-login.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+//receive username and passowrd
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+//check admin table for valid username and password
+$dsn = "mysql:host=localhost;dbname=immnewsnetwork.com;charset=utf8mb4";
+$dbusername = "converyj";
+$dbpassword = "HUgT86Fga#97";
+
+$pdo = new PDO($dsn, $dbusername, $dbpassword); 
+// check if user exists if not add them to the user table
+$stmt = $pdo->prepare("
+	SELECT * FROM `user` 
+	WHERE `username` = '$username' 
+	AND `password` = '$password'");
+
+$stmt->execute();
+
+if($row = $stmt->fetch()){
+	//start session if valid and redirect to dashboard
+	$_SESSION['logged-in'] = true;
+	$_SESSION['username'] = $row['username'];
+	// $_SESSION['role'] = $row['role'];
+	$_SESSION['userID'] = $row['userID'];
+
+	header("Location: dashboard.php");
+
+}else{
+	$stmt = $pdo->prepare("
+	INSERT INTO `user` (`username`, `password`)
+	VALUES ('$username', '$password') ");
+
+$stmt->execute();
+}
+?>
